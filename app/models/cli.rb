2dcd7bc33d3976ee192016228774ad0187f12195
CodyC <codyicastro@gmail.com>
2020-07-28 15:33:22 -0400

    ready for CLI

---
+++ app/models/cli.rb
@@ -0,0 +1,11 @@
+class CLI
+
+ def start
+    roommate = Roommate.first method in roommate
+    if Roommate
+        puts ""
+    else
+        puts ""
+        Roommate.______
+
+end
---
+++ tools/console.rb
@@ -0,0 +1,12 @@
+require_relative ' ./config/environment'
+
+
+
+
+
+
+
+
+
+binding.pry
+0
